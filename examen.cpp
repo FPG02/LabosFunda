90c0844950662172d6acda5cfbd80234ecc77154
FPG02 <00155220@uca.edu.sv>
2020-05-20 13:42:38 -0600

    .

---
+++ examen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int x = 320;
+    int y = 0;
+    int z = 0;
+
+    float operacion = x = x%73 ;
+    cout<<"La operacion es "<<operacion;
+
+    return 0;
+}
